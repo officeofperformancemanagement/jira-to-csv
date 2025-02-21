3a622148b9de76281724c0c4a0a5e1233a538192
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2025-02-21 11:03:26 -0500

    added columns param and updated documentation

--- README.md
+++ README.md
@@ -27,18 +27,24 @@ const { csv } = jiraToCsv.export_issues({
     api_token: "71236DSFg6gq566D123u67",
     user_email: "user@example.org",
 
-    // optional parameters below
+    // all parameters below are optional
     debug_level: 10,
 
     // custom jql (can only be specified if project parameter above is undefined)
     jql: "...",
 
-    // only write each column if it actually contains values. default is false
-    skipEmptyColumns: true,
+    // specify columns to include in output
+    // columns not in this array will be excluded from the output
+    columns: ["ID", "Summary", "Time to resolution: Elapsed Time"],
 
+    // only write each column if it actually contains values. default is false
+    drop_empty_columns: true,
+
+    // default is 2000
     // how many mimilliseconds to wait between requests
     wait: 50,
 
+    // default is false
     // split time in status information across multiple columns
     // each status has two columns: count and duration
     expand_time_in_status: true,
@@ -47,11 +53,19 @@ const { csv } = jiraToCsv.export_issues({
     // overflow is replaced by "..."
     max_description_length: 100,
 
-    // enable experimental memory optimization
-    // uses memory-optimizer internally to replace
-    // duplicate values in issue fields with
-    // multiple references to the same object
-    optimize_memory: true
+    // number of issues to skip
+    offset: 12345,
+
+    // remove all new lines from field values, ensuring each row takes up only one line
+    remove_new_lines: false,
+
+    // default is semi-colon
+    // separator between multiple values within one "cell"
+    subdelimiter: "|",
+
+    // default is true
+    // whether to sort values within a cell
+    sort: false
 });
 
 fs.writeFileSync("./issues.csv", csv);
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const { convert } = require("@danieljdufour/json-to-csv");
-const memoryOptimizer = require("memory-optimizer");
+const sendero = require("sendero");
+const memsafe = require("memsafe");
+const papaparse = require("papaparse");
 
 const sleep = ms => new Promise(resolve => setTimeout(() => resolve(), ms));
 
@@ -99,7 +100,7 @@ async function get_fields({ api_token, custom = false, domain, debug_level = 0, 
 }
 
 async function export_issues({
-  columns,
+  columns: column_names,
   debug_level = false,
   domain,
   headers,
@@ -112,8 +113,11 @@ async function export_issues({
   wait = 2000, // sleep between requests,
   expand_time_in_status = false, // expand time in status value to multiple columns
   max_description_length,
-  optimize_memory = false,
-  ...rest
+  drop_empty_columns = false,
+  subdelimiter = "; ",
+  sort: _sort = true,
+  remove_new_lines = false,
+  offset = 0
 }) {
   if (typeof fetch !== "function") {
     throw new Error("[jira-to-csv] it looks like fetch is undefined. Try upgrading NodeJS to a more recent version.");
@@ -145,70 +149,90 @@ async function export_issues({
 
   const status_by_id = Object.fromEntries(statuses.map(status => [status.id, status]));
 
-  let time_in_status_field_id = custom_fields.find(field => field.name === "[CHART] Time in Status")?.id;
+  const time_in_status_field_id = custom_fields.find(field => field.name === "[CHART] Time in Status")?.id;
+  if (debug_level >= 3) console.log(`[jira-to-csv] time_in_status_field_id: ${time_in_status_field_id}`);
 
-  if (!columns) {
-    columns = [
-      { name: "ID", path: "id" },
-      { name: "Key", path: "key" },
-      { name: "URL", path: "self" },
-      { name: "Created", path: "fields.created" },
-      { name: "Summary", path: "fields.summary" },
-      { name: "Description", path: "fields.description" },
-      { name: "Labels", path: "fields.labels" },
-      { name: "Assignee Name", path: "fields.assignee.displayName" },
-      { name: "Assignee Email", path: "fields.assignee.emailAddress" },
-      { name: "Creator Name", path: "fields.creator.displayName" },
-      { name: "Creator Email", path: "fields.creator.emailAddress" },
-      { name: "Due Date", path: "fields.duedate" },
-      { name: "Issue Type Name", path: "fields.issuetype.name" },
-      { name: "Issue Type Description", path: "fields.issuetype.description" },
-      { name: "Project ID", path: "fields.project.id" },
-      { name: "Project Key", path: "fields.project.key" },
-      { name: "Project Name", path: "fields.project.name" },
-      { name: "Reporter Name", path: "fields.reporter.displayName" },
-      { name: "Reporter Email", path: "fields.reporter.emailAddress" },
-      { name: "Status Category Change Date", path: "fields.statuscategorychangedate" }
-    ];
+  let columns = [
+    { name: "ID", path: "id" },
+    { name: "Key", path: "key" },
+    { name: "URL", path: "self" },
+    { name: "Created", path: "fields.created" },
+    { name: "Summary", path: "fields.summary" },
+    { name: "Description", path: "fields.description" },
+    { name: "Labels", path: "fields.labels" },
+    { name: "Assignee Name", path: "fields.assignee.displayName" },
+    { name: "Assignee Email", path: "fields.assignee.emailAddress" },
+    { name: "Creator Name", path: "fields.creator.displayName" },
+    { name: "Creator Email", path: "fields.creator.emailAddress" },
+    { name: "Due Date", path: "fields.duedate" },
+    { name: "Issue Type Name", path: "fields.issuetype.name" },
+    { name: "Issue Type Description", path: "fields.issuetype.description" },
+    { name: "Project ID", path: "fields.project.id" },
+    { name: "Project Key", path: "fields.project.key" },
+    { name: "Project Name", path: "fields.project.name" },
+    { name: "Reporter Name", path: "fields.reporter.displayName" },
+    { name: "Reporter Email", path: "fields.reporter.emailAddress" },
+    { name: "Status Category Change Date", path: "fields.statuscategorychangedate" }
+  ];
 
-    for (let i = 0; i < custom_fields.length; i++) {
-      const { name, id, schema } = custom_fields[i];
-      if (schema.type === "user") {
-        columns.push({ name: name + " Name", path: ["fields", id, "displayName"].join(".") });
-        columns.push({ name: name + " Email", path: ["fields", id, "emailAddress"].join(".") });
-      } else if (schema.type === "array" && schema.items === "user") {
-        columns.push({ name: name + " Names", path: ["fields", id, "displayName"].join(".") });
-        columns.push({ name: name + " Emails", path: ["fields", id, "emailAddress"].join(".") });
-      } else if (schema.type === "array" && schema.items === "option") {
-        columns.push({ name, path: ["fields", id, "value"].join(".") });
-      } else if (schema.type === "option") {
-        columns.push({ name, path: ["fields", id, "value"].join(".") });
-      } else if (schema.type === "sd-request-lang" && schema.custom === "com.atlassian.servicedesk.servicedesk-lingo-integration-plugin:sd-request-language") {
-        columns.push({ name: name + " Code", path: ["fields", id, "languageCode"].join(".") });
-        columns.push({ name: name + " Name", path: ["fields", id, "displayName"].join(".") });
-      } else if (schema.type === "sd-servicelevelagreement" && schema.custom === "com.atlassian.servicedesk:sd-sla-field") {
-        columns.push({ name: name + ": Elapsed Time", path: ["fields", id, "completedCycles", "elapsedTime", "millis"] });
-        columns.push({ name: name + ": Goal Duration", path: ["fields", id, "completedCycles", "goalDuration", "millis"] });
-      } else if (schema.type === "sd-customerrequesttype" && schema.custom === "com.atlassian.servicedesk:vp-origin") {
-        columns.push({ name: name + ": Current Status", path: ["fields", id, "currentStatus", "status"] });
-      } else if (schema.type === "any" && schema.custom === "com.atlassian.jira.ext.charting:timeinstatus" && expand_time_in_status) {
-        statuses.forEach(status => {
-          columns.push({ name: `${name} Count: ${status.name}`, path: ["fields", id, status.name, "count"] });
-          columns.push({ name: `${name} Duration: ${status.name}`, path: ["fields", id, status.name, "duration"] });
-        });
-      } else {
-        columns.push({ name, path: "fields." + id });
-      }
+  for (let i = 0; i < custom_fields.length; i++) {
+    const { name, id, schema } = custom_fields[i];
+    const same_name_count = columns.filter(col => col.name === name).length + custom_fields.slice(0, i).filter(col => col.name === name).length;
+    const suffix = same_name_count === 0 ? "" : ` (${same_name_count + 1})`;
+    const column_name = name + suffix;
+    if (schema.type === "user") {
+      columns.push({ name: column_name + " Name", path: ["fields", id, "displayName"].join(".") });
+      columns.push({ name: column_name + " Email", path: ["fields", id, "emailAddress"].join(".") });
+    } else if (schema.type === "array" && schema.items === "user") {
+      columns.push({ name: column_name + " Names", path: ["fields", id, "displayName"].join(".") });
+      columns.push({ name: column_name + " Emails", path: ["fields", id, "emailAddress"].join(".") });
+    } else if (schema.type === "array" && schema.items === "option") {
+      columns.push({ name: column_name, path: ["fields", id, "value"].join(".") });
+    } else if (schema.type === "option") {
+      columns.push({ name: column_name, path: ["fields", id, "value"].join(".") });
+    } else if (schema.type === "sd-request-lang" && schema.custom === "com.atlassian.servicedesk.servicedesk-lingo-integration-plugin:sd-request-language") {
+      columns.push({ name: column_name + " Code", path: ["fields", id, "languageCode"].join(".") });
+      columns.push({ name: column_name + " Name", path: ["fields", id, "displayName"].join(".") });
+    } else if (schema.type === "sd-servicelevelagreement" && schema.custom === "com.atlassian.servicedesk:sd-sla-field") {
+      columns.push({ name: column_name + ": Elapsed Time", path: ["fields", id, "completedCycles", "elapsedTime", "millis"], aggregate: "sum" });
+      columns.push({ name: column_name + ": Goal Duration", path: ["fields", id, "completedCycles", "goalDuration", "millis"], aggregate: "max" });
+    } else if (schema.type === "sd-customerrequesttype" && schema.custom === "com.atlassian.servicedesk:vp-origin") {
+      columns.push({ name: column_name + ": Current Status", path: ["fields", id, "currentStatus", "status"] });
+    } else if (schema.type === "any" && schema.custom === "com.atlassian.jira.ext.charting:timeinstatus" && expand_time_in_status) {
+      statuses.forEach(status => {
+        columns.push({ name: `${column_name} Count: ${status.name}`, path: ["fields", id, status.name, "count"] });
+        columns.push({ name: `${column_name} Duration: ${status.name}`, path: ["fields", id, status.name, "duration"] });
+      });
+    } else {
+      columns.push({ name: column_name, path: "fields." + id });
     }
+  }
+
+  if (column_names && column_names.length > 0) {
+    if (debug_level >= 3) console.log(`[jira-to-csv] filtering by column names`);
+    columns = columns.filter(col => column_names.includes(col.name));
+
+    // if any column names specified that aren't in columns
+    const missing_column_names = column_names.filter(name => columns.filter(c => c.name === name).length === 0);
+    missing_column_names.forEach(name => {
+      columns.push({ name, path: null });
+      if (debug_level >= 3) console.log(`[jira-to-csv] adding empty column:`, name);
+    });
+
+    // re-sort based on order specified
+    columns.sort((a, b) => Math.sign(column_names.indexOf(a.name) - column_names.indexOf(b.name)));
+    if (debug_level >= 3) console.log(`[jira-to-csv] sorted columns:`, columns);
   }
 
   if (debug_level >= 3) console.log(`[jira-to-csv] columns:`, columns);
 
   const url = domain + "/rest/api/2/search";
 
-  let all_issues = [];
+  const df = new memsafe.MemSafeTable({
+    column_names: columns.map(col => col.name)
+  });
 
-  let startAt = 0;
+  let startAt = offset;
   for (let i = 0; i < max_requests; i++) {
     if (debug_level >= 3) console.log(`[jira-to-csv] request number: ${i}`);
 
@@ -254,10 +278,6 @@ async function export_issues({
 
     const response_issues = response_data.issues;
 
-    if (optimize_memory) {
-      memoryOptimizer.optimize(response_issues);
-    }
-
     if (typeof max_description_length === "number") {
       response_issues.forEach(issue => {
         if (typeof issue.fields.description === "string" && issue.fields.description.length > max_description_length) {
@@ -268,7 +288,8 @@ async function export_issues({
 
     // preprocess issues
     response_issues.forEach(response_issue => {
-      if (expand_time_in_status && time_in_status_field_id && response_issue.fields[time_in_status_field_id] !== null) {
+      // sometimes the time in status can be a null string like "null"
+      if (expand_time_in_status && time_in_status_field_id && ["null", null, "", undefined].indexOf(response_issue.fields[time_in_status_field_id]) === -1) {
         const time_in_status = response_issue.fields[time_in_status_field_id];
         if (debug_level >= 5) console.log("[jira-to-csv] time_in_status:", time_in_status);
         response_issue.fields[time_in_status_field_id] = Object.fromEntries(
@@ -293,17 +314,47 @@ async function export_issues({
 
     if (response_issues.length === 0) break;
 
-    all_issues = all_issues.concat(response_issues);
-
-    if (optimize_memory) {
-      memoryOptimizer.optimize(all_issues);
-    }
-
-    if (typeof max_issues === "number" && max_issues !== Infinity) {
-      if (all_issues.length >= max_issues) {
-        all_issues = all_issues.slice(0, max_issues);
+    for (let r = 0; r < response_issues.length; r++) {
+      const issue = response_issues[r];
+      if (typeof max_issues === "number" && max_issues !== Infinity && df.length === max_issues) {
         break;
       }
+
+      // need to convert to save memory
+      const row = Object.fromEntries(
+        columns.map(col => {
+          // if path is null return empty string
+          // this can happen when the user asks for a column that doesn't exist (or may have existed previously)
+          if (col.path === null) return [col.name, ""];
+
+          let value = sendero.get(issue, col.path, { clean: true, sort: _sort, stringify: true, unique: false });
+
+          if (value.length === 1 && value[0] === "{{issue.reporter.emailAddress}}") {
+            const colpath = "fields.reporter.emailAddress";
+            const original = value;
+            value = sendero.get(issue, colpath, { clean: true, sort: _sort, stringify: true, unique: false });
+            if (debug_level >= 2) console.log("[jira-to-csv] replaced", original, "with", value);
+          }
+
+          if (col.aggregate === "sum") {
+            value = value.reduce((acc, it) => acc + Number(it), 0).toString();
+          } else if (col.aggregate === "max" && value.length > 1) {
+            value = Math.max(...value.map(v => Number(v))).toString();
+          } else {
+            value = value.join(subdelimiter);
+          }
+          if (remove_new_lines) value = value.replace(/(\n|\r|\n\r|\r\n)/g, " ");
+          value = value.trim();
+          if (value.includes("EPIC_LINK_SHOULD_BE_USED")) value = "";
+          return [col.name, value];
+        })
+      );
+
+      df.push(row);
+    }
+
+    if (typeof max_issues === "number" && max_issues !== Infinity && df.length === max_issues) {
+      break;
     }
 
     startAt += response_issues.length;
@@ -311,13 +362,36 @@ async function export_issues({
 
   if (debug_level >= 5) console.warn("[jira-to-csv] memory usage:", process.memoryUsage());
 
-  const csv = convert(all_issues, {
-    columns,
-    ...rest,
-    debug: false,
-    quotes: true,
-    start: "."
+  // proactively clean empty columns lowering future memory consumption
+  const drop_these_columns = [];
+  if (drop_empty_columns) {
+    columns.forEach(col => {
+      const rle = df._columns.find(it => it[0] === col.name)[1]._data;
+      if (rle.length === 2 && [undefined, ""].includes(rle[1])) {
+        drop_these_columns.push(col.name);
+      }
+    });
+  }
+
+  if (drop_these_columns.length > 0) {
+    columns = columns.filter(col => drop_these_columns.indexOf(col.name) === -1);
+    drop_these_columns.forEach(col => {
+      df.drop_column(col);
+      if (debug_level >= 3) console.log(`[jira-to-csv] dropped empty column "${col}"`);
+    });
+  }
+
+  const rows = [];
+  for (const row of df) {
+    rows.push(row);
+  }
+
+  const csv = papaparse.unparse(rows, {
+    columns: columns.map(col => col.name),
+    newline: "\n",
+    quotes: true
   });
+
   if (debug_level >= 3) console.log("[jira-to-csv] csv header:", csv.split("\n")[0].split(","));
 
   if (debug_level >= 3) {
